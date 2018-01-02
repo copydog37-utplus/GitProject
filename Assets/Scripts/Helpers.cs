202b400c4a8a2a50ef8f596e4f72cc5dc1207098
[클라이언트]최해준 <copydog37@utplusnf.co.kr>
2018-01-02 18:32:54 +0900

    test

--- Assets/Scripts/Helpers.cs
+++ Assets/Scripts/Helpers.cs
@@ -2,6 +2,7 @@
 
 public class Helpers
 {
+	//Test
     // This sets the layer of all renderer children of a given gameobject (including itself)
     // Useful to make an object display only on a single camera (e.g. character on UI cam on loadout State)
     static public void SetRendererLayerRecursive(GameObject root, int layer)
